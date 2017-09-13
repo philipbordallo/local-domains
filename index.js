294407f246093c9491a56a4d241afcac92f04e00
Philip Bordallo <git@philipbordallo.com>
2017-09-12 23:11:19 -0700

    Add better IP detection

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const os = require('os');
 const DEFAULT_TYPES = ['dev', 'meh', 'xip', 'name.xip', 'www.name.xip'];
 
 function localDomains(name, types = DEFAULT_TYPES) {
-	const ip = os.networkInterfaces().en0.filter(networkInterface => networkInterface.family === 'IPv4')[0];
+	const interfaces = os.networkInterfaces();
+	const ip = Object.keys(interfaces)
+		.reduce((a, item) => a.concat(interfaces[item]), [])
+		.find(({ internal, family }) => !internal && family === 'IPv4');
 
 	return types.map(type => {
 		if (type === 'xip' || type === 'xip.io') {
